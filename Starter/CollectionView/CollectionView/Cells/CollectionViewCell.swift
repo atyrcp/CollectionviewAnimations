2e2ec6d6929951918b2742e87210b9a0e79e32f1
atyrcp <atyrcp@gmail.com>
2019-05-27 11:05:30 +0800

    custom cells and put a imageView on it instead of just a label

--- Starter/CollectionView/CollectionView/Cells/CollectionViewCell.swift
+++ Starter/CollectionView/CollectionView/Cells/CollectionViewCell.swift
@@ -1,14 +1,27 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
-	@IBOutlet weak var titleLabel:UILabel!
-	@IBOutlet weak var selectionImage:UIImageView!
-	
+	@IBOutlet private weak var titleLabel:UILabel!
+	@IBOutlet private weak var selectionImage:UIImageView!
+    @IBOutlet private weak var mainImageView: UIImageView!
+    
+    var park: Park? {
+        didSet {
+            if let park = park {
+                mainImageView.image = UIImage(named: park.photo)
+            }
+        }
+    }
+    
 	var isEditing: Bool = false {
 		didSet {
 			selectionImage.isHidden = !isEditing
 		}
 	}
+    
+    override func prepareForReuse() {
+        mainImageView.image = nil
+    }
 	
 	override var isSelected: Bool {
 		didSet {
